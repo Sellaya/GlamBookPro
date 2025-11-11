157f11e7192aec724a0f582f9cfa4f00834a294b
Shan Ali <shaneali720@gmail.com>
2025-11-11 18:30:08 +0000

    what are the issues we are facing in order to make our application run

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarInset,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Welcome to GlamBookPro</h1>
-    </main>
+    <>
+      <Sidebar>
+        <SidebarHeader>
+          <SidebarTrigger />
+        </SidebarHeader>
+        <SidebarContent>
+          {/* Sidebar content goes here */}
+        </SidebarContent>
+      </Sidebar>
+      <SidebarInset>
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+          <h1 className="text-4xl font-bold">Welcome to GlamBookPro</h1>
+        </main>
+      </SidebarInset>
+    </>
   );
 }
